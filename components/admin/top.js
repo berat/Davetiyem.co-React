41a4cc73c7529beea1eea8dc7e6f406399b1f87d
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-03-21 11:48:40 +0300

    layout üst tarafı bitti

--- components/admin/top.js
+++ components/admin/top.js
@@ -1,15 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+import Cookies from 'js-cookie'
+import jwtDecode from 'jwt-decode'
 import Link from 'next/link'
+import config from '../../config'
 
 import images from '../../public/images/image'
 
 const Top = () => {
+  const [username, setUsername] = useState()
 
-  // Axios ile aktifHesap usernameyi  çek token gönder
+  const userid =
+    Cookies.get('login') != undefined
+      ? jwtDecode(Cookies.get('login')).userid
+      : null
+
+  useEffect(() => {
+    Axios.get(`http://${config.apiURL}${config.version}uyeCek`).then(
+      response => {
+        if (
+          response.data.uyeler.filter(item => item.userid == userid).length == 1
+        ) {
+          setUsername(response.data.uyeler.filter(item => item.userid == userid)[0].username)
+        }
+      }
+    )
+  }, [setUsername])
+
 
   return (
     <div className="row justify-content-md-center">
       <div className="siteyiGoruntule col-lg-3 col-sm-12 col-md-12">
-        <Link href="/damatgelin">
+        <Link href={`/${username}`}>
           <a className="form-control">Davetiyeyi Görüntüle</a>
         </Link>
       </div>
@@ -21,7 +43,7 @@ const Top = () => {
         </Link>
       </div>
       <div className="cikisYap col-lg-3 col-sm-12 col-md-12">
-        <Link href="/cikis">
+        <Link href="/logout">
           <a className="form-control">Çıkış Yap</a>
         </Link>
       </div>
@@ -29,4 +51,4 @@ const Top = () => {
   )
 }
 
-export default Top;
+export default Top
