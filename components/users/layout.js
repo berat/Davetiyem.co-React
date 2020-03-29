dd890112d7b1833f82d08692d40e990102614136
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-03-29 21:41:45 +0300

    hotjar kodu eklendi

--- components/users/layout.js
+++ components/users/layout.js
@@ -158,6 +158,18 @@ const Layout = ({ children, userid }) => {
           `
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:1285342,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+          }}
+        />
       </Head>
       <Header
         kisisel={kisisel}
