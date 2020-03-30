d0f4a8b80e125c6c367a883407f50de372f1f2c3
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-03-30 13:43:38 +0300

    progress bar deactive edildi

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../public/fonts/font.css'
-import NextNProgress from './progressNextnp'
+// import NextNProgress from './progressNextnp'
 import Router from 'next/router'
 import * as gtag from '../lib/gtag'
 
@@ -9,7 +9,7 @@ Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <NextNProgress />
+      {/* <NextNProgress /> */}
       <Component {...pageProps} />
     </>
   )
