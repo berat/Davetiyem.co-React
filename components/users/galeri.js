f75756cac6ab2039eb87c9d827499e563046a7a4
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-06-19 01:00:59 +0300

    album lightbox eklendi

--- components/users/galeri.js
+++ components/users/galeri.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Swiper from 'react-id-swiper'
 import useWindowSize from '../../lib/hooks'
 import 'swiper/css/swiper.css'
+import Carousel, { Modal, ModalGateway } from 'react-images';
 
 const Galeri = ({ galeri, username }) => {
+  const [light, setLight] = React.useState({isVisible: false, index: 0})
   const size = useWindowSize()
 
   const params = {
@@ -15,14 +17,30 @@ const Galeri = ({ galeri, username }) => {
       hide: false
     }
   }
+
+  console.log(galeri)
+
   return galeri.length != 0 ? (
     <div id="galeriSlider">
+       <ModalGateway>
+        {light.isVisible ? (
+          <Modal onClose={() => setLight({isVisible: !light.isVisible, index: 0})}>
+            <Carousel currentIndex={light.index} views={galeri.map(item => {return {source: `/uploads/users/${username}/${item.foto}`}})} />
+          </Modal>
+        ) : null}
+      </ModalGateway>
       <Swiper {...params}>
-        {galeri.map(item => (
+        {galeri.map((item, index) => (
           <img
             key={item.fotoid}
             src={`/uploads/users/${username}/${item.foto}`}
-            width={(size.width / 3.9) + 50}
+            width={size.width / 3.9 + 50}
+            onClick={() =>
+              setLight({
+                isVisible: !light.isVisible,
+                index: index
+              })
+            }
             height={size.width / 3.9}
             alt=""
           />
